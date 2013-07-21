0eca491dc66e04f7be58f67270f88eb52d5f4bd7
Elenor <github@elenor.net>
2013-07-21 08:50:54 -0700

    PSR formatting

--- htdocs/env.php
+++ htdocs/env.php
@@ -1,6 +1,6 @@
 <?php
-	//set the error reporting level to report all errors
-	error_reporting(-1);
-	
-	//set the default timezone
-	date_default_timezone_set(@date_default_timezone_get());
+    //set the error reporting level to report all errors
+    error_reporting(-1);
+    
+    //set the default timezone
+    date_default_timezone_set(@date_default_timezone_get());
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,74 +1,74 @@
 <?php
-	//everything below can be overridden by an environmental file (this can also be auto-prepended by the server)
-	(file_exists('env.php') && include_once 'env.php');
-	
-	//define the environment type and the source root
-	(!defined('PHK_ENV')) && define('PHK_ENV', 'prod');
-	(!defined('PHK_ROOT')) && define('PHK_ROOT', dirname(__DIR__).DIRECTORY_SEPARATOR);
-	
-	
-	//create a temporary closure to define file paths
-	$pathdef = function($constant, $path) {
-		if (!defined($constant)) {
-			(!is_dir($path) && is_dir(PHK_ROOT.$path)) && $path = PHK_ROOT.$path;
-			define($constant, realpath($path).DIRECTORY_SEPARATOR);
-		}
-	};
-	
-	//define the file paths
-	$pathdef('CORE_PATH', 'php/core');
-	$pathdef('APP_PATH', 'php/app');
-	$pathdef('VIEW_PATH', 'php/app/views');
-	$pathdef('PKG_PATH', 'php/pkg');
-	$pathdef('LOG_PATH', 'logs');
-	
-	//the path definer has served its purpose
-	unset($pathdef);
-	
-	
-	//create a temporary closure to import core and optionally app files
-	empty($import) && $import = function($path, $type = 'classes') {
-		$path = str_replace('/', DIRECTORY_SEPARATOR, $path);
-		
-		if (is_file(CORE_PATH.$type.DIRECTORY_SEPARATOR.$path.'.php')) {
-			require_once CORE_PATH.$type.DIRECTORY_SEPARATOR.$path.'.php';
-		}
-		
-		if (is_file(APP_PATH.$type.DIRECTORY_SEPARATOR.$path.'.php')) {
-			require_once APP_PATH.$type.DIRECTORY_SEPARATOR.$path.'.php';
-		}
-	};
-	
-	//import the absolute basic core and app files
-	$import('singleton');
-	$import('exception');
-	$import('loader');
-	$import('event');
-	$import('bootstrap');
-	
-	//the import function should be replaced with the loader class after this point
-	unset($import);
-	
-	
-	//call the optional post-load / pre-run function
-	function_exists('phork_initialize') && phork_initialize();
-	
-	
-	//create a class alias to either the core class or the app class for each main component
-	(!class_exists('PhorkException')) && class_alias('Phork\\Core\\Exception', 'PhorkException');
-	(!class_exists('PhorkLoader')) && class_alias('Phork\\Core\\Loader', 'PhorkLoader');
-	(!class_exists('PhorkEvent')) && class_alias('Phork\\Core\\Event', 'PhorkEvent');
-	(!class_exists('Phork')) && class_alias('Phork\\App\\Bootstrap', 'Phork');
-	
-	//initialize the bootstrap, register the common objects, initialize the app, and run everything
-	try {
-		Phork::instance()
-		     ->register('loader', PhorkLoader::instance(true))
-		     ->register('event', PhorkEvent::instance(true))
-		     ->init(PHK_ENV)
-		     ->run()
-		     ->shutdown()
-		;
-	} catch (Exception $exception) {
-		require 'fatal.php';
-	}
+    //everything below can be overridden by an environmental file (this can also be auto-prepended by the server)
+    (file_exists('env.php') && include_once 'env.php');
+    
+    //define the environment type and the source root
+    (!defined('PHK_ENV')) && define('PHK_ENV', 'prod');
+    (!defined('PHK_ROOT')) && define('PHK_ROOT', dirname(__DIR__).DIRECTORY_SEPARATOR);
+    
+    
+    //create a temporary closure to define file paths
+    $pathdef = function($constant, $path) {
+        if (!defined($constant)) {
+            (!is_dir($path) && is_dir(PHK_ROOT.$path)) && $path = PHK_ROOT.$path;
+            define($constant, realpath($path).DIRECTORY_SEPARATOR);
+        }
+    };
+    
+    //define the file paths
+    $pathdef('CORE_PATH', 'php/core');
+    $pathdef('APP_PATH', 'php/app');
+    $pathdef('VIEW_PATH', 'php/app/views');
+    $pathdef('PKG_PATH', 'php/pkg');
+    $pathdef('LOG_PATH', 'logs');
+    
+    //the path definer has served its purpose
+    unset($pathdef);
+    
+    
+    //create a temporary closure to import core and optionally app files
+    empty($import) && $import = function($path, $type = 'classes') {
+        $path = str_replace('/', DIRECTORY_SEPARATOR, $path);
+        
+        if (is_file(CORE_PATH.$type.DIRECTORY_SEPARATOR.$path.'.php')) {
+            require_once CORE_PATH.$type.DIRECTORY_SEPARATOR.$path.'.php';
+        }
+        
+        if (is_file(APP_PATH.$type.DIRECTORY_SEPARATOR.$path.'.php')) {
+            require_once APP_PATH.$type.DIRECTORY_SEPARATOR.$path.'.php';
+        }
+    };
+    
+    //import the absolute basic core and app files
+    $import('singleton');
+    $import('exception');
+    $import('loader');
+    $import('event');
+    $import('bootstrap');
+    
+    //the import function should be replaced with the loader class after this point
+    unset($import);
+    
+    
+    //call the optional post-load / pre-run function
+    function_exists('phork_initialize') && phork_initialize();
+    
+    
+    //create a class alias to either the core class or the app class for each main component
+    (!class_exists('PhorkException')) && class_alias('Phork\\Core\\Exception', 'PhorkException');
+    (!class_exists('PhorkLoader')) && class_alias('Phork\\Core\\Loader', 'PhorkLoader');
+    (!class_exists('PhorkEvent')) && class_alias('Phork\\Core\\Event', 'PhorkEvent');
+    (!class_exists('Phork')) && class_alias('Phork\\App\\Bootstrap', 'Phork');
+    
+    //initialize the bootstrap, register the common objects, initialize the app, and run everything
+    try {
+        Phork::instance()
+             ->register('loader', PhorkLoader::instance(true))
+             ->register('event', PhorkEvent::instance(true))
+             ->init(PHK_ENV)
+             ->run()
+             ->shutdown()
+        ;
+    } catch (Exception $exception) {
+        require 'fatal.php';
+    }
